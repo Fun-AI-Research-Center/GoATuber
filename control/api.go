4827b0ee9c43d0990c45a524711d772d8ddd3f00
Lv Yonghuan <110887355+lvyonghuan@users.noreply.github.com>
2023-08-21 21:14:10 +0800

    release alpha
    我有点太急了

--- control/api.go
+++ control/api.go
@@ -1,9 +1,37 @@
 package control
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
 func initControlRouter() {
 	r := gin.Default()
+
+	//加载静态控制器页面
+	r.Use(static.Serve("/", static.LocalFile("./control/frontend", true)))
+	r.NoRoute(func(c *gin.Context) {
+		accept := c.Request.Header.Get("Accept")
+		flag := strings.Contains(accept, "text/html")
+		if flag {
+			content, err := os.ReadFile("./control/frontend/index.html")
+			if err != nil {
+				c.Writer.WriteHeader(404)
+				c.Writer.WriteString("Not Found")
+				return
+			}
+			c.Writer.WriteHeader(200)
+			c.Writer.Header().Add("Accept", "text/html")
+			c.Writer.Write(content)
+			c.Writer.Flush()
+		}
+	})
+
 	//获取配置文件信息
 	r.GET("/init", getConfig)
 
@@ -40,5 +68,11 @@ func initControlRouter() {
 	r.GET("/run", run)
 	//终止进程
 	r.GET("/stop", stop)
+
+	//调用浏览器打开前端页面
+	cmd := exec.Command("cmd", "/c", "start", "http://127.0.0.1:8900")
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.Start()
+
 	r.Run(":8900")
 }
